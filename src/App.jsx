364aaf305352f3351580d0b00e2ba2ba6b36d541
Tyler <tyler.tomczyk@gmail.com>
2019-03-13 20:01:17 +0000

    initial breakout of static html started. Rendering started but not complete.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+//is this correct or will it be import or picked up somewhere else? Looks like yes from Karl lecture
+//need to consider whether it should be function (no state etc.) otherwise 'class extend'
+require('../styles/home.scss')
+
+//not 100% sure this is correct location yet
+import ChatBar from './ChatBar.jsx'
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello Reactttt :)</h1>
+      <div>
+        <main className="messages">
+          <div className="message">
+            <span className="message-username">Anonymous1</span>
+            <span className="message-content">I won't be impressed with technology until I can download food.</span>
+          </div>
+          <div className="message system">
+            Anonymous1 changed their name to nomnom.
+          </div>
+        </main>
+        < ChatBar />
+      </div>
     );
   }
 }
